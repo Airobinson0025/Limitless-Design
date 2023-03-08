6c98abf9c01271a954e18404e532c72db6a4d1ed
Aaron Robinsom <airobinson0025@gmail.com>
2023-03-08 11:43:33 -0500

    initial commit

---
+++ pages/components/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex items-center justify-center h-screen bg-fixed bg-center bg-cover custom-img'>
+
+        
+        <div className='text-center sm:text-left sm:mt-96 sm:ml-96 space-y-5'>
+            <h1 className='text-3xl sm:text-5xl font-playfair tracking-wide'>Limitless Design</h1>
+            <p className='font-redhat text-md sm:text-lg tracking-[8px]'>GRAPHIC DESIGN</p>
+            <button className='font-redhat text-md sm:text-lg border-2 border-black rounded-md py-2 px-4 tracking-wider hover:border-transparent hover:text-white hover:bg-[#A45483] transition hover:translate-y-1 active:scale-90 duration-300'>Book Appointment</button>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Hero
---
+++ pages/components/Navbar.js
@@ -0,0 +1,92 @@
+import Link from 'next/link'
+import React, { useState } from 'react'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+
+const Navbar = () => {
+
+    const [ dropMenu, setDropMenu ] = useState(false)
+
+    const handleClick = () => {
+        setDropMenu(!dropMenu)
+    }
+
+
+
+  return (
+    <div className='fixed w-full flex justify-between items-center p-8'>
+
+    <div>
+        <h1 className='invisible sm:visible sm:text-xl font-semibold'>Logo Here</h1>
+    </div>
+    
+    <div>
+        <ul className='hidden sm:flex space-x-8 sm:text-[1.8rem] font-semibold font-dark tracking-wider'>
+            <li className='hover:scale-90 transition duration-200'>
+                <Link href='/' className='hover:text-[#A45483] transition duration-200'>Home</Link>
+            </li>
+            <li className='hover:scale-90 transition duration-200'>
+                <Link href='/about' className='hover:text-[#A45483] transition duration-200'>About</Link>
+            </li>
+            <li className='hover:scale-90 transition duration-200'>
+                <Link href='/templates' className='hover:text-[#A45483] transition duration-200'>Templates</Link>
+            </li>
+            <li className='hover:scale-90 transition duration-200'>
+                <Link href='/blog' className='hover:text-[#A45483] transition duration-200'>Blog</Link>
+            </li>
+            <li className='hover:scale-90 transition duration-200'>
+                <Link href='/contact' className='hover:text-[#A45483] transition duration-200'>Contact</Link>
+            </li>
+        </ul>
+    </div>
+
+    
+    <div className='sm:hidden'>
+        <button onClick={handleClick} className='border-2 border-black py-2 px-2 rounded-full'>
+        {dropMenu ? null : <AiOutlineMenu size={25}/>}
+        </button>
+    </div>
+
+    {/* mobile menu */}
+
+    <div className={dropMenu ? 'sm:hidden absolute top-0 bottom-0 left-0 right-0 flex justify-center items-center text-center w-full h-screen bg-white ease-in duration-300' : 'sm:hidden absolute top-0 bottom-0 left-[-1000px] right-0 flex justify-center items-center text-center w-full h-screen bg-white ease-in duration-300' }>
+
+    <button onClick={handleClick} className='border-2 border-black py-2 px-2 rounded-full absolute top-[30px] right-[30px]'>
+        {dropMenu ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25}/>}
+    </button>
+        
+        
+        <ul className='space-y-5 text-3xl font-dark'>
+            <li>
+                <Link href='/' className='hover:text-[#A45483] transition duration-200'>Home</Link>
+            </li>
+            <li>
+                <Link href='/about' className='hover:text-[#A45483] transition duration-200'>About</Link>
+            </li>
+            <li>
+                <Link href='/templates' className='hover:text-[#A45483] transition duration-200'>Templates</Link>
+            </li>
+            <li>
+                <Link href='/blog' className='hover:text-[#A45483] transition duration-200'>Blog</Link>
+            </li>
+            <li>
+                <Link href='/contact' className='hover:text-[#A45483] transition duration-200'>Contact</Link>
+            </li>
+        </ul>
+    </div>
+
+    
+    
+    
+
+       
+    
+    
+
+
+    
+    
+    </div>
+  )
+}
+
+export default Navbar
