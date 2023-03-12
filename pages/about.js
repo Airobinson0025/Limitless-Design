a551d54bb0957993dda48ab370e18a1f98506818
Aaron Robinsom <airobinson0025@gmail.com>
2023-03-12 11:42:58 -0400

    about change

--- pages/about.js
+++ pages/about.js
@@ -47,7 +47,7 @@ const About = () => {
                 }
             }
         }}  className='mx-6 font-dark sm:text-[1.4rem] tracking-wider max-w-[750px] bg-white p-6 mb-12 rounded-xl shadow-2xl shadow-neutral-500'>
-            <h1 className='font-vibes font-bold text-[3.7rem] sm:text-[4.5rem] text-center text-[#A24857] tracking-[3px] pt-4 mb-8 '>About Us</h1>
+            <h1 className='font-vibes font-bold text-[3.7rem] sm:text-[4.5rem] text-center text-[#A24857] pt-4 mb-8 '>About Us</h1>
 
             <div className='text-stone-900 text-[1.3rem] sm:text-[1.4rem] space-y-5  sm:border-neutral-600 p-6'>
             
